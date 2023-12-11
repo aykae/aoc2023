d6f419f73033a9e9335c33ca3aa2d32dcd400a9e
aykae <raiaboudi@yahoo.com>
2023-12-11 15:48:14 -0500

    began 5

--- 5/5.cpp
+++ 5/5.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
+#include <vector>
+#include <array>
 
 int main() {
     std::ifstream file("input.txt"); // replace with your file name
@@ -10,7 +13,36 @@ int main() {
     }
 
     std::string line;
+    std::getline(file, line);
+
+
+    std::istringstream iss1(line);
+    std::vector<long> seeds;
+    std::string seed;
+    std::getline(iss1, seed, ' ');
+    while(std::getline(iss1, seed, ' ')) {
+        std::cout << seed << std::endl;
+        seeds.push_back(std::stol(seed));
+    }
+
+    std::vector<std::vector<long>> mappings;
+    int mapIdx = -1;
     while (std::getline(file, line)) {
+        if (std::isdigit(line[0])) {
+            std::istringstream iss2(line);
+
+            std::string num;
+            std::getline(iss2, num, ' ');
+            mappings[mapIdx].push_back(std::stol(num));
+            std::getline(iss2, num, ' ');
+            mappings[mapIdx].push_back(std::stol(num));
+            std::getline(iss2, num, ' ');
+            mappings[mapIdx].push_back(std::stol(num));
+        }
+        else if (std::isalpha(line[0])) {
+            stdA::asdfkkkj
+            mapIdx += 1;
+        }
         std::cout << line << std::endl;
     }
 
